37a31348493af26cba5db2dcbc6bfe877140ba56
HaruYuki-Ar1ta <Campbcha000@warren.k12.in.us>
2022-02-12 02:29:38 -0500

    ClassObjectAssignment Completed

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rect1 = new Rectangle(25.3, 30.2);
+
+        System.out.println(rect1.getLength() + " " + rect1.getWidth());
+        rect1.setLength(11.5);
+        rect1.setWidth(22.956);
+        System.out.println(rect1.getLength() + " " + rect1.getWidth());
+
+
+
+
+    }
+}
